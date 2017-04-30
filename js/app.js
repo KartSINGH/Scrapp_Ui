29c072859176e4137ea2a65c7ff89e89f4bd59c3
kartik singh <kart.singh15@gmail.com>
2017-04-30 10:59:53 +0530

    OTP verification Added

--- js/app.js
+++ js/app.js
@@ -8,12 +8,49 @@ app.config(function ($httpProvider) {
 }])*/
 
 //login Controller
-app.controller('otpCtrl',function($scope){
-$scope.user = {};
-$scope.signup = function(){
+app.controller('otpCtrl', function ($scope,$http,$rootScope) {
+    $scope.user = {};
+    $scope.signup = function () {
+
+        console.log("logincontroller called");
+        console.log($scope.user);
+        var user_number = $scope.user.user_number;
+
+        $http({
+            method: 'POST',
+            url: 'https://sendotp.msg91.com/api/generateOTP',
+            headers: {
+                "application-Key": "w7gEzSbCuU7xnHloQyf1Cq8SFtbEBOq0aShp5EVo2iJ6-IleyhL7ZVCIIGasO2_EfcDk-4YgwahdGaTQf9nRSD3KIflNgjB0QKyqAEGgUlhfQPwSHp4HqzlabTtSiYydAZafinYK8GsUudgYOkQeXw=="
+            },
+            data: {
+                countryCode: 91,
+                mobileNumber: user_number,
+                getGeneratedOTP: true
+            }
+        }).then(function (data) {
+
+            $rootScope.s_otp = data.data.response.oneTimePassword;
+            console.log($rootScope.s_otp);
+
+           // $state.go("otp-enter");
+
+
+        }, function (error) {
+            console.log("error");
+            alert("error occured");
+        })
+    }
+    $scope.verify_otp=function(){
+        console.log("Your Otp is getting under verification");
+        console.log($scope.user_otp);
+        var isVerified;
+        if($scope.user_otp==$rootScope.s_otp){
+            console.log("OTP has been verified")
+            
+        }else{
+           alert("Wrong OTP.Please try again")
+           
+        }
+    }
     
-  console.log("logincontroller called");
-    console.log($scope.user);
-    var user_number = $scope.user_number;
-}
 });

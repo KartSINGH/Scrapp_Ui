0c89fc93588f8f7d16839db04bf2150ff81d76d8
kartik singh <kart.singh15@gmail.com>
2017-05-20 10:35:33 +0530

    Image Refinements

--- js/app.js
+++ js/app.js
@@ -166,7 +166,7 @@ app.run(function ($rootScope, $state, $timeout) {
 app.controller('otpCtrl', function ($scope, $http, $rootScope, $state) {
     $rootScope.user_data = {};
     $rootScope.user = {};
-  
+    
     $scope.signup = function () {
         console.log("otp controller called");
         console.log($rootScope.user_data);

efcfcbc5fb6a98a54842e1da014e59ee12ec005f
kartik singh <kart.singh15@gmail.com>
2017-05-22 15:56:34 +0530

    SmoothScroll Added in angular

--- js/app.js
+++ js/app.js
@@ -75,6 +75,59 @@ app.service('googleService', ['$http', '$rootScope', '$q', function ($http, $roo
     }
 
 }]);
+app.service('anchorSmoothScroll', function(){
+    
+    this.scrollTo = function(eID) {
+
+        // This scrolling function 
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+        
+        var startY = currentYPosition();
+        var stopY = elmYPosition(eID);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i=startY; i>stopY; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+        
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop)
+                return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+        
+        function elmYPosition(eID) {
+            var elm = document.getElementById(eID);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            } return y;
+        }
+
+    };
+    
+});
 
 app.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $localStorageProvider) {
     $locationProvider.html5Mode(true);
@@ -84,6 +137,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $htt
         .state('landingPage', {
             url: "/",
             templateUrl: "../templates/welcome.html",
+            controller:"otpCtrl"
 
         })
         .state('register', {
@@ -168,10 +222,19 @@ app.run(function ($rootScope, $state, $timeout) {
     })
 });
 //otp Controller
-app.controller('otpCtrl', function ($scope, $http, $rootScope, $state) {
+app.controller('otpCtrl', function ($scope, $http, $rootScope, $state,$location,anchorSmoothScroll) {
     $rootScope.user_data = {};
     $rootScope.user = {};
     //$state.go('login');
+    $scope.gotoElement = function (eID){
+      // set the location.hash to the id of
+      // the element you wish to scroll to.
+      $location.hash('bottom');
+ 
+      // call $anchorScroll()
+      anchorSmoothScroll.scrollTo(eID);
+      
+    };
     $scope.signup = function () {
         console.log("otp controller called");
         console.log($rootScope.user_data);
